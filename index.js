da45cac37844dec7ab7bf9d71ca12b9633ba5de8
alstjd223 <13hj45@gmail.com>
2024-11-27 11:03:28 +0900

    api 삭제

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://broker.emqx.io:1883');
-const { sequelize, User, Cardkey } = require('./db');
+const {
+    sequelize,
+    User,
+    Cardkey
+} = require('./db');
 
 const app = express();
 
@@ -9,7 +13,6 @@ const RFID_TOPIC = "rfid/scan";
 const RESPONSE_TOPIC = "rfid/response";
 
 let exists = 0;
-let cardnum = "CDF46360";
 
 client.on('connect', () => {
     client.subscribe([RFID_TOPIC], (err) => {
@@ -25,13 +28,15 @@ client.on('message', async (topic, message) => {
     let messageStr = message.toString();
 
     if (topic === RFID_TOPIC) {
-        cardnum = messageStr.replace(/\s+/g, '');
+        let cardnum = messageStr.replace(/\s+/g, '');
         const doorid = cardnum[0];
         cardnum = cardnum.slice(1);
 
         try {
             const card = await Cardkey.findOne({
-                where: { cardnum },
+                where: {
+                    cardnum
+                },
                 include: User
             });
 
@@ -43,51 +48,41 @@ client.on('message', async (topic, message) => {
                 exists = 0;
             }
 
-            client.publish(RESPONSE_TOPIC, exists.toString(), (err) => {
-                if (err) {
-                    console.error('MQTT publish error', err);
+            if (exists === 1) {
+                client.publish(RESPONSE_TOPIC, '1', (err) => {
+                    if (err) {
+                        console.error('MQTT publish error', err);
+                    }
+                });
+
+                const card = await Cardkey.findOne({
+                    where: {
+                        cardnum
+                    }
+                });
+
+                if (card) {
+                    const usercode = card.usercode;
+                    const del = await User.update({
+                        doorpermission: 0
+                    }, {
+                        where: {
+                            usercode
+                        }
+                    });
                 } else {
-                    console.log(`Sent response: ${exists} for cardnum: ${cardnum}`);
+                    console.log(`No card found for cardnum: ${cardnum}`);
                 }
-            });
+            } else {
+                client.publish(RESPONSE_TOPIC, '0', (err) => {
+                    if (err) {
+                        console.error('MQTT publish error', err);
+                    }
+                });
+            }
         } catch (err) {
             console.error('Database query error', err);
         }
-    }
-});
-
-app.get('/permission', async (req, res) => {
-    if (exists === 1) {
-        client.publish(RESPONSE_TOPIC, '1', (err) => {
-            if (err) {
-                console.error('MQTT publish error', err);
-            }
-        });
-
-        const card = await Cardkey.findOne({
-            where: { cardnum }
-        });
-
-        if (card) {
-            const usercode = card.usercode;
-            const del = await User.update(
-                { doorpermission: 0 },
-                {
-                    where: { usercode }
-                }
-            );
-        } else {
-            console.log(`No card found for cardnum: ${cardnum}`);
-        }
-
-        res.status(200).send('문이 열렸습니다.');
-    } else {
-        client.publish(RESPONSE_TOPIC, '0', (err) => {
-            if (err) {
-                console.error('MQTT publish error', err);
-            }
-        });
-        res.status(403).send('권한이 없습니다.');
     }
 });
 
